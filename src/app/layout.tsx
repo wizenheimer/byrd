f4bbe8fd1c693c5acfab46cbaa83e4ad6fba9069
wizenheimer <xNayanKumar@gmail.com>
2024-10-24 02:49:46 +0530

    feat: add core page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased mb-24`}
+				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
 				<Navbar />
 				{children}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
+import BigPictureSection from "@/components/BigPictureSection";
+import CompetitionSection from "@/components/Competition";
+import DifferentiationSection from "@/components/DifferentiationSection";
+import ExitCTA from "@/components/ExitCTA";
+import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
+import LastOne from "@/components/LastOne";
+import MonitoringSection from "@/components/MonitoringSection";
+import TestimonialsSection from "@/components/Testimonials";
 
 export default function Home() {
 	return (
 		<>
-		<HeroSection />
+			<HeroSection />
+			<MonitoringSection />
+			<BigPictureSection />
+			<LastOne />
+			<CompetitionSection />
+			<DifferentiationSection />
+			<TestimonialsSection />
+			<ExitCTA />
+			<Footer />
 		</>
 	);
 }
