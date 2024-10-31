c87856996eb2a12d92a7ba0ef208285c8f3b3071
wizenheimer <xNayanKumar@gmail.com>
2024-10-31 14:52:19 +0530

    chore: synchronize onboarding assets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
-import Navbar from "@/components/Navbar";
-import HeroSection from "@/components/sections/HeroSection";
-import ContextSection from "@/components/sections/ContextSection";
-import TestimonialsSection from "@/components/sections/TestimonialSection";
-import MonitoringSection from "@/components/sections/MonitoringSection";
-import InboxSection from "@/components/sections/InboxSection";
-import MediaIntelligenceSection from "@/components/sections/MediaIntelligenceSection";
-import CustomerIntelligenceSection from "@/components/sections/CustomerIntelligenceSection";
-import IntegrationSection from "@/components/sections/IntegrationSection";
-import ScaleupSection from "@/components/sections/ScaleupSection";
-import ClosingNoteSection from "@/components/sections/ClosingNoteSection";
 import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+import ClosingNoteSection from "@/components/sections/ClosingNoteSection";
+import ContextSection from "@/components/sections/ContextSection";
+import CustomerIntelligenceSection from "@/components/sections/CustomerIntelligenceSection";
+import HeroSection from "@/components/sections/HeroSection";
+import InboxSection from "@/components/sections/InboxSection";
+import IntegrationSection from "@/components/sections/IntegrationSection";
+import MediaIntelligenceSection from "@/components/sections/MediaIntelligenceSection";
+import MonitoringSection from "@/components/sections/MonitoringSection";
+import ScaleupSection from "@/components/sections/ScaleupSection";
+import TestimonialsSection from "@/components/sections/TestimonialSection";
 
 export default function Home() {
 	return (
---
+++ src/components/block/OnboardingPreviewPane.tsx
@@ -0,0 +1,18 @@
+type Props = {
+	imageSrc: string;
+	altText?: string;
+};
+
+const OnboardingPreviewPane = ({ imageSrc, altText }: Props) => {
+	return (
+		<div className="hidden md:block md:w-1/3 lg:w-1/2 bg-gray-50 overflow-hidden relative">
+			<img
+				src={imageSrc}
+				alt={altText || "Dashboard Preview"}
+				className="absolute top-0 left-0 w-auto h-full object-cover object-left pl-8 pt-6 pb-6"
+			/>
+		</div>
+	);
+};
+
+export default OnboardingPreviewPane;
