3bc1dad0ffc7ec974bdbac5f19604c5d397e1124
wizenheimer <xNayanKumar@gmail.com>
2024-12-13 02:34:22 +0530

    feat: embed dedicated and shared client into service during initialization

--- src/server/startup.go
+++ src/server/startup.go
@@ -3,9 +3,9 @@ package main
 import (
 	"database/sql"
 	"log"
-	"net/http"
 
 	"github.com/wizenheimer/iris/src/internal/api/routes"
+	"github.com/wizenheimer/iris/src/internal/client"
 	"github.com/wizenheimer/iris/src/internal/config"
 	"github.com/wizenheimer/iris/src/internal/domain/interfaces"
 	"github.com/wizenheimer/iris/src/internal/repository/db"
@@ -22,21 +22,55 @@ import (
 func initializer(cfg *config.Config, sqlDb *sql.DB, logger *logger.Logger) (*routes.HandlerContainer, error) {
 	logger.Debug("initializing services", zap.Any("service_config", zap.Any("config", cfg.Services)), zap.Any("storage_config", cfg.Storage), zap.Any("database_config", cfg.Database), zap.Any("server_config", cfg.Server), zap.Any("environment_config", cfg.Environment))
 
-	// Initialize HTTP client
-	httpClient := &http.Client{}
+	// Initialize HTTP client for services
+	screenshotClientOpts := []client.ClientOption{
+		client.WithLogger(logger),
+		client.WithAuth(client.BearerAuth{
+			Token: cfg.Services.ScreenshotServiceAPIKey,
+		}),
+	}
+	screenshotHttpClient, err := client.NewClient(screenshotClientOpts...)
+	if err != nil {
+		return nil, err
+	}
+
+	// Create a new rate limited client
+	// This client will be used to make requests to the screenshot service
+	screenshotRateLimitedClient := client.NewRateLimitedClient(screenshotHttpClient, cfg.Services.ScreenshotServiceQPS)
+
+	aiClientOpts := []client.ClientOption{
+		client.WithLogger(logger),
+		client.WithAuth(client.BearerAuth{
+			Token: cfg.Services.OpenAIKey,
+		}),
+	}
+
+	aiHttpClient, err := client.NewClient(aiClientOpts...)
+	if err != nil {
+		return nil, err
+	}
+
+	commonClientOpts := []client.ClientOption{
+		client.WithLogger(logger),
+	}
+
+	commonHttpClient, err := client.NewClient(commonClientOpts...)
+	if err != nil {
+		return nil, err
+	}
 
 	// Initialize services
-	screenshotService, err := setupScreenshotService(cfg, httpClient, logger)
+	screenshotService, err := setupScreenshotService(cfg, screenshotRateLimitedClient, logger)
 	if err != nil {
 		return nil, err
 	}
 
-	notificationService, err := setupNotificationService(cfg, httpClient, logger)
+	notificationService, err := setupNotificationService(cfg, commonHttpClient, logger)
 	if err != nil {
 		return nil, err
 	}
 
-	diffService, err := setupDiffService(cfg, sqlDb, httpClient, logger)
+	diffService, err := setupDiffService(cfg, sqlDb, screenshotRateLimitedClient, aiHttpClient, logger)
 	if err != nil {
 		return nil, err
 	}
@@ -58,7 +92,7 @@ func initializer(cfg *config.Config, sqlDb *sql.DB, logger *logger.Logger) (*rou
 	return handlers, nil
 }
 
-func setupScreenshotService(cfg *config.Config, httpClient interfaces.HTTPClient, logger *logger.Logger) (interfaces.ScreenshotService, error) {
+func setupScreenshotService(cfg *config.Config, screenshotHTTPClient client.HTTPClient, logger *logger.Logger) (interfaces.ScreenshotService, error) {
 	storageRepo, err := storage.NewR2Storage(
 		cfg.Storage.AccessKey,
 		cfg.Storage.SecretKey,
@@ -70,19 +104,22 @@ func setupScreenshotService(cfg *config.Config, httpClient interfaces.HTTPClient
 		logger.Fatal("Failed to initialize storage", zap.Error(err))
 	}
 
+	// Create screenshot service option
 	screenshotServiceOptions := []screenshot.ScreenshotServiceOption{
 		screenshot.WithStorage(storageRepo),
-		screenshot.WithHTTPClient(httpClient),
+		screenshot.WithHTTPClient(screenshotHTTPClient),
 		screenshot.WithKey(cfg.Services.ScreenshotServiceAPIKey),
+		screenshot.WithOrigin(cfg.Services.ScreenshotServiceOrigin),
 	}
 
+	// Create a new screenshot service
 	return screenshot.NewScreenshotService(
 		logger,
 		screenshotServiceOptions...,
 	)
 }
 
-func setupNotificationService(cfg *config.Config, httpClient *http.Client, logger *logger.Logger) (interfaces.NotificationService, error) {
+func setupNotificationService(cfg *config.Config, httpClient client.HTTPClient, logger *logger.Logger) (interfaces.NotificationService, error) {
 	emailClient, err := notification.NewResendEmailClient(cfg, httpClient, logger)
 	if err != nil {
 		log.Fatalf("Failed to initialize email client: %v", err)
@@ -96,13 +133,13 @@ func setupNotificationService(cfg *config.Config, httpClient *http.Client, logge
 	return notification.NewNotificationService(emailClient, templateManager, logger)
 }
 
-func setupDiffService(cfg *config.Config, sqlDb *sql.DB, httpClient *http.Client, logger *logger.Logger) (interfaces.DiffService, error) {
-	screenshotService, err := setupScreenshotService(cfg, httpClient, logger)
+func setupDiffService(cfg *config.Config, sqlDb *sql.DB, screenshotHTTPClient, aiHTTPClient client.HTTPClient, logger *logger.Logger) (interfaces.DiffService, error) {
+	screenshotService, err := setupScreenshotService(cfg, screenshotHTTPClient, logger)
 	if err != nil {
 		log.Fatalf("Failed to initialize screenshot service: %v", err)
 	}
 
-	aiService, err := ai.NewOpenAIService(cfg.Services.OpenAIKey, httpClient, logger)
+	aiService, err := ai.NewOpenAIService(cfg.Services.OpenAIKey, aiHTTPClient, logger)
 	if err != nil {
 		log.Fatalf("Failed to initialize AI service: %v", err)
 	}
