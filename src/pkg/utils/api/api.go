3009e7f8ec91fcccc6fd9f45487cca97e4ec31e9
wizenheimer <xNayanKumar@gmail.com>
2024-12-13 21:53:05 +0530

    feat: add api utils

---
+++ src/pkg/utils/api/api.go
@@ -0,0 +1,16 @@
+package api
+
+import (
+	"bytes"
+	"image"
+	"image/png"
+)
+
+// WritePNGResponse writes an image to a PNG byte array
+func WritePNGResponse(img image.Image) ([]byte, error) {
+	buffer := new(bytes.Buffer)
+	if err := png.Encode(buffer, img); err != nil {
+		return nil, err
+	}
+	return buffer.Bytes(), nil
+}
