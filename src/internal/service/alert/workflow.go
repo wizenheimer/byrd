87f1faa2b78b8e3cbcbf0a484ab693fbc1aa2f4a
wizenheimer <xNayanKumar@gmail.com>
2024-12-17 05:50:55 +0530

    feat: add workflow alert

---
+++ src/internal/service/alert/workflow.go
@@ -0,0 +1,66 @@
+package alert
+
+import (
+	"context"
+
+	"github.com/wizenheimer/iris/src/internal/domain/interfaces"
+	"github.com/wizenheimer/iris/src/internal/domain/models"
+	"github.com/wizenheimer/iris/src/pkg/logger"
+)
+
+type workflowAlertClient struct {
+	// client for Slack
+	interfaces.AlertClient
+	// logger for logging
+	logger *logger.Logger
+}
+
+func NewWorkflowAlertClient(config models.SlackConfig, logger *logger.Logger) (interfaces.WorkflowAlertClient, error) {
+	embeddedClient, err := NewSlackAlertClient(config, logger)
+	if err != nil {
+		return nil, err
+	}
+
+	workflowAlertClient := workflowAlertClient{
+		AlertClient: embeddedClient,
+		logger:      logger.WithFields(map[string]interface{}{"module": "workflow_alert_client"}),
+	}
+
+	return &workflowAlertClient, nil
+}
+
+// SendWorkflowStarted sends an alert when a workflow starts
+func (w *workflowAlertClient) SendWorkflowStarted(
+	ctx context.Context,
+	id models.WorkflowIdentifier,
+	details map[string]string,
+) error {
+	return w.sendWorkflowAlert(ctx, id, EventStarted, details)
+}
+
+// SendWorkflowCompleted sends an alert when a workflow completes
+func (w *workflowAlertClient) SendWorkflowCompleted(
+	ctx context.Context,
+	id models.WorkflowIdentifier,
+	details map[string]string,
+) error {
+	return w.sendWorkflowAlert(ctx, id, EventCompleted, details)
+}
+
+// SendWorkflowFailed sends an alert when a workflow fails
+func (w *workflowAlertClient) SendWorkflowFailed(
+	ctx context.Context,
+	id models.WorkflowIdentifier,
+	details map[string]string,
+) error {
+	return w.sendWorkflowAlert(ctx, id, EventFailed, details)
+}
+
+// SendWorkflowCancelled sends an alert when a workflow is cancelled
+func (w *workflowAlertClient) SendWorkflowCancelled(
+	ctx context.Context,
+	id models.WorkflowIdentifier,
+	details map[string]string,
+) error {
+	return w.sendWorkflowAlert(ctx, id, EventCancelled, details)
+}
