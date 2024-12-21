bac5f0de66a4e373cfc619a1bcd795f16fdce575
wizenheimer <xNayanKumar@gmail.com>
2024-12-21 13:52:30 +0530

    feat: add concrete diff service implementation

--- src/internal/service/diff/service.go
+++ src/internal/service/diff/service.go
@@ -2,6 +2,7 @@ package diff
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/wizenheimer/iris/src/internal/domain/interfaces"
 	"github.com/wizenheimer/iris/src/internal/domain/models"
@@ -10,10 +11,10 @@ import (
 )
 
 type diffService struct {
-	diffRepo   interfaces.DiffRepository
-	aiService  interfaces.AIService
-	// screenshot interfaces.ScreenshotService
-	logger     *logger.Logger
+	diffRepo  interfaces.DiffRepository
+	aiService interfaces.AIService
+	processor *MarkdownProcessor
+	logger    *logger.Logger
 }
 
 func NewDiffService(
@@ -24,56 +25,68 @@ func NewDiffService(
 ) (interfaces.DiffService, error) {
 	logger.Debug("creating new diff service")
 
+	// Initialize markdown processor
+	processor, err := NewMarkdownProcessor()
+	if err != nil {
+		return nil, fmt.Errorf("failed to create new markdown processor: %w", err)
+	}
+
 	// Initialize diff service
 	diffService := &diffService{
-		diffRepo:   diffRepo,
-		aiService:  aiService,
-		// screenshot: screenshot,
-		logger:     logger.WithFields(map[string]interface{}{"module": "diff_service"}),
+		diffRepo:  diffRepo,
+		aiService: aiService,
+		processor: processor,
+		logger:    logger.WithFields(map[string]interface{}{"module": "diff_service"}),
 	}
 	return diffService, nil
 }
 
-// CreateDiff creates a diff for between any 2 versions of a URL
+// Get creates a diff for between any 2 versions of a URL
 // This is used when both versions are available in the storage via screenshotService
-func (d *diffService) GetDiffAnalysis(ctx context.Context, req models.URLDiffRequest) (*models.URLDiffAnalysis, error) {
+func (d *diffService) Get(ctx context.Context, req models.URLDiffRequest) (*models.DynamicChanges, error) {
 	d.logger.Debug("creating diff", zap.Any("url", req.URL), zap.Any("week_day_1", req.WeekDay1), zap.Any("week_number_1", req.WeekNumber1), zap.Any("week_day_2", req.WeekDay2), zap.Any("week_number_2", req.WeekNumber2))
-	// Implementation
-	return nil, nil
+	return d.diffRepo.Get(ctx, req)
 }
 
-// CreateReport creates a weekly report for the given competitor
-// This is used when the competitor has multiple URLs and each URL has multiple available versions
-func (d *diffService) CreateReport(ctx context.Context, req models.WeeklyReportRequest) (*models.WeeklyReport, error) {
-	d.logger.Debug("creating report", zap.Any("week_number", req.WeekNumber), zap.Any("week_day_1", req.WeekDay1), zap.Any("week_day_2", req.WeekDay2), zap.Any("urls", req.URLs))
-	// Implementation
-	return nil, nil
-}
+// Compare compares the contents of 2 screenshots
+// This is used when the screenshots are available in memory
+// If the AI analysis is not available, it falls back to the default diff analysis
+func (d *diffService) Compare(ctx context.Context, content1, content2 *models.ScreenshotHTMLContentResponse, profileStr string, persist bool) (*models.DynamicChanges, error) {
+	d.logger.Debug("comparing screenshot contents", zap.Any("content_1", len(content1.HTMLContent)), zap.Any("content_2", len(content2.HTMLContent)))
+	// Attempt to get the AI analysis for the screenshot contents if available
+	req := models.URLDiffRequest{
+		URL:         content1.Metadata.SourceURL,
+		WeekDay1:    content1.Metadata.WeekDay,
+		WeekNumber1: content1.Metadata.WeekNumber,
+		WeekDay2:    content2.Metadata.WeekDay,
+		WeekNumber2: content2.Metadata.WeekNumber,
+	}
+	aiAnalysis, err := d.Get(ctx, req)
+	if err == nil {
+		return aiAnalysis, nil
+	}
+	// If not available, fallback to the default diff analysis
+	markdownContent1, err := d.processor.Process(content1.HTMLContent)
+	if err != nil {
+		return nil, fmt.Errorf("failed to process markdown content 1: %w", err)
+	}
 
-// CompareImageResponse compares 2 image responses and returns the ImageResponseDiff
-func (d *diffService) CompareImageResponse(ctx context.Context, img1, img2 *models.ScreenshotImageResponse) (*models.ImageResponseDiff, error) {
-	d.logger.Debug("comparing image responses", zap.Any("image_1", img1), zap.Any("image_2", img2))
-	// Implementation
-	return nil, nil
-}
+	markdownContent2, err := d.processor.Process(content2.HTMLContent)
+	if err != nil {
+		return nil, fmt.Errorf("failed to process markdown content 2: %w", err)
+	}
 
-// CompareHTMLContentResponse compares 2 HTML content responses and returns the HTMLContentResponseDiff
-func (d *diffService) CompareHTMLContentResponse(ctx context.Context, content1, content2 *models.ScreenshotHTMLContentResponse) (*models.HTMLContentResponseDiff, error) {
-	d.logger.Debug("comparing HTML content responses", zap.Any("content_1", content1), zap.Any("content_2", content2))
-	// Implementation
-	return nil, nil
-}
+	aiAnalysis, err = d.aiService.AnalyzeContentDifferences(ctx, markdownContent1, markdownContent2, profileStr)
+	if err != nil {
+		return nil, fmt.Errorf("failed to analyze content differences: %w", err)
+	}
 
-// CompareScreenshotContents compares the contents of 2 screenshots and returns the URLDiffAnalysis
-func (d *diffService) CompareScreenshotContents(ctx context.Context, content1, content2 *models.ScreenshotHTMLContentResponse) (*models.URLDiffAnalysis, error) {
-	d.logger.Debug("comparing screenshot contents", zap.Any("content_1", content1), zap.Any("content_2", content2))
-	// Implementation
-	return nil, nil
-}
+	if persist {
+		// Save the AI analysis for future use
+		if err = d.diffRepo.Set(ctx, req, aiAnalysis); err != nil {
+			return nil, fmt.Errorf("failed to save diff analysis: %w", err)
+		}
+	}
 
-// CompareScreenshotImages compares the images of 2 screenshots and returns the URLDiffAnalysis
-func (d *diffService) CompareScreenshotImages(ctx context.Context, img1, img2 *models.ScreenshotImageResponse) (*models.URLDiffAnalysis, error) {
-	d.logger.Debug("comparing screenshot images", zap.Any("image_1", img1), zap.Any("image_2", img2))
-	// Implementation
-	return nil, nil
+	return aiAnalysis, nil
 }
