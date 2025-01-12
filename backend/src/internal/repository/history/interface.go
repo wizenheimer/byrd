75a03e39c58926b072acd796ef7879e059261c22
wizenheimer <xNayanKumar@gmail.com>
2025-01-12 18:54:26 +0530

    fix: refine history repository

--- backend/src/internal/repository/history/interface.go
+++ backend/src/internal/repository/history/interface.go
@@ -11,17 +11,9 @@ import (
 // This is used to interact with the page history repository
 
 type PageHistoryRepository interface {
-	// ---- CRUD operations for page history ----
+	CreateHistoryForPage(ctx context.Context, pageID uuid.UUID, pageHistory models.PageHistory) (*models.PageHistory, error)
 
-	// CreatePageHistory creates a new page history.
-	// The page history is created with the provided page ID and page history.
-	CreateHistoryForPage(ctx context.Context, pageID uuid.UUID, pageHistory models.PageHistory) (models.PageHistory, error)
-
-	// BatchGetPageHistory lists page history for a page ordered by created at.
-	// When limit and offset are nil, all page history is returned.
 	BatchGetPageHistory(ctx context.Context, pageID uuid.UUID, limit, offset *int) ([]models.PageHistory, error)
 
-	// RemovePageHistory removes page history for a list of pages.
-	// Returns an error if pageIDs are nil.
 	BatchRemovePageHistory(ctx context.Context, pageIDs []uuid.UUID) error
 }
--- backend/src/internal/repository/history/repository.go
+++ backend/src/internal/repository/history/repository.go
@@ -21,20 +21,14 @@ func NewPageHistoryRepository(tm *transaction.TxManager, logger *logger.Logger) 
 	}
 }
 
-// CreatePageHistory creates a new page history.
-// The page history is created with the provided page ID and page history.
-func (r *historyRepo) CreateHistoryForPage(ctx context.Context, pageID uuid.UUID, pageHistory models.PageHistory) (models.PageHistory, error) {
-	return models.PageHistory{}, nil
+func (r *historyRepo) CreateHistoryForPage(ctx context.Context, pageID uuid.UUID, pageHistory models.PageHistory) (*models.PageHistory, error) {
+	return nil, nil
 }
 
-// BatchGetPageHistory lists page history for a page ordered by created at.
-// When limit and offset are nil, all page history is returned.
 func (r *historyRepo) BatchGetPageHistory(ctx context.Context, pageID uuid.UUID, limit, offset *int) ([]models.PageHistory, error) {
 	return []models.PageHistory{}, nil
 }
 
-// RemovePageHistory removes page history for a list of pages.
-// Returns an error if pageIDs are nil.
 func (r *historyRepo) BatchRemovePageHistory(ctx context.Context, pageIDs []uuid.UUID) error {
 	return nil
 }
