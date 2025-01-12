044c07c026eb44ad8bc79529be112c13242fff82
wizenheimer <xNayanKumar@gmail.com>
2025-01-12 18:55:10 +0530

    fix: refresh workspace repository

--- backend/src/internal/repository/workspace/interface.go
+++ backend/src/internal/repository/workspace/interface.go
@@ -11,78 +11,41 @@ import (
 // This is used to interact with the workspace repository
 
 type WorkspaceRepository interface {
-	// ------ CRUD operations for workspace ------
+	CreateWorkspace(ctx context.Context, workspaceName, billingEmail string, workspaceCreatorUserID uuid.UUID) (*models.Workspace, error)
 
-	// ---- Create operations for workspace ----
+	WorkspaceExists(ctx context.Context, workspaceID uuid.UUID) (bool, error)
 
-	// CreateWorkspace creates a new workspace.
-	// The workspace is created with the provided name and billing email.
-	CreateWorkspace(ctx context.Context, workspaceName, billingEmail string, workspaceCreatorUserID uuid.UUID) (models.Workspace, error)
+	GetWorkspaceByWorkspaceID(ctx context.Context, workspaceID uuid.UUID) (*models.Workspace, error)
 
-	// ---- Read operations for workspace ----
-
-	// GetWorkspaceByWorkspaceID gets a workspace by its ID.
-	// This is used to get the workspace details.
-	GetWorkspaceByWorkspaceID(ctx context.Context, workspaceID uuid.UUID) (models.Workspace, error)
-
-	// BatchGetWorkspacesByIDs gets workspaces by their IDs.
-	// This is used to get the workspace details.
 	BatchGetWorkspacesByIDs(ctx context.Context, workspaceIDs []uuid.UUID) ([]models.Workspace, error)
 
-	// GetWorkspaceUserIDs gets the users of the workspace.
-	// This is used to get the users of the workspace.
-	GetWorkspaceUserIDs(ctx context.Context, workspaceID uuid.UUID) ([]uuid.UUID, error)
+	GetWorkspaceMembers(ctx context.Context, workspaceID uuid.UUID) ([]models.PartialWorkspaceUser, error)
 
-	// GetWorkspaceUserCountByRole gets the count of users by role in the workspace.
-	// This is used to get the count of users by role in the workspace.
-	GetWorkspaceUserCountByRole(ctx context.Context, workspaceID uuid.UUID) (map[models.UserWorkspaceRole]int, error)
+	GetWorkspaceMemberByUserID(ctx context.Context, workspaceID, userID uuid.UUID) (*models.PartialWorkspaceUser, error)
 
-	// GetWorkspaceForUser gets the workspace for a user.
-	// This is used to get the workspace for a user.
-	GetWorkspaceForUser(ctx context.Context, userID, workspaceID uuid.UUID) (models.Workspace, error)
+	GetWorkspaceUserCountByRole(ctx context.Context, workspaceID uuid.UUID) (map[models.WorkspaceRole]int, error)
 
-	// ---- Update operations for workspace ----
+	GetWorkspacesForUserID(ctx context.Context, userID uuid.UUID) ([]models.Workspace, error)
 
-	// User Specific Update Operations
+	BatchAddUsersToWorkspace(ctx context.Context, userIDs []uuid.UUID, workspaceID uuid.UUID) ([]models.PartialWorkspaceUser, error)
 
-	// BatchAddUsersToWorkspace adds a batch of users to workspace
-	BatchAddUsersToWorkspace(ctx context.Context, userIDs []uuid.UUID, workspaceID uuid.UUID) ([]models.WorkspaceUser, error)
+	AddUserToWorkspace(ctx context.Context, userID, workspaceID uuid.UUID) (*models.PartialWorkspaceUser, error)
 
-	// AddUserToWorkspace adds a user to a workspace
-	AddUserToWorkspace(ctx context.Context, userID, workspaceID uuid.UUID) (models.WorkspaceUser, error)
-
-	// BatchRemoveUsersFromWorkspace removes a batch of users from a workspace
 	BatchRemoveUsersFromWorkspace(ctx context.Context, userIDs []uuid.UUID, workspaceID uuid.UUID) error
 
-	// RemoveUserFromWorkspace removes a user from a workspace
 	RemoveUserFromWorkspace(ctx context.Context, userID, workspaceID uuid.UUID) error
 
-	// Membership Specific Update Operations
+	UpdateUserRoleForWorkspace(ctx context.Context, workspaceID, userID uuid.UUID, role models.WorkspaceRole) error
 
-	// UpdateUserRoleForWorkspace updates the role of a user in the workspace
-	UpdateUserRoleForWorkspace(ctx context.Context, workspaceID, userID uuid.UUID, role models.UserWorkspaceRole) error
+	UpdateUserMembershipStatusForWorkspace(ctx context.Context, workspaceID, userID uuid.UUID, status models.MembershipStatus) error
 
-	// Workspace Specific Update Operations
-
-	// UpdateWorkspaceBillingEmail updates the billing email of the workspace.
-	// This is used to update the billing email of the workspace.
 	UpdateWorkspaceBillingEmail(ctx context.Context, workspaceID uuid.UUID, billingEmail string) error
 
-	// UpdateWorkspaceName updates the name of the workspace.
-	// This is used to update the name of the workspace.
 	UpdateWorkspaceName(ctx context.Context, workspaceID uuid.UUID, workspaceName string) error
 
-	// UpdateWorkspaceDetails updates the details of the workspace.
-	// This is used to update the details of the workspace.
 	UpdateWorkspaceDetails(ctx context.Context, workspaceID uuid.UUID, workspaceName, billingEmail string) error
 
-	// ---- Delete operations for workspace ----
-
-	// DeleteWorkspace deletes a workspace by its ID.
-	// This is used to delete the workspace.
 	DeleteWorkspace(ctx context.Context, workspaceID uuid.UUID) error
 
-	// BatchDeleteWorkspaces deletes workspaces by their IDs.
-	// This is used to delete the workspaces.
 	BatchDeleteWorkspaces(ctx context.Context, workspaceIDs []uuid.UUID) error
 }
--- backend/src/internal/repository/workspace/workspace.go
+++ backend/src/internal/repository/workspace/workspace.go
@@ -21,105 +21,78 @@ func NewWorkspaceRepository(tm *transaction.TxManager, logger *logger.Logger) Wo
 	}
 }
 
-// CreateWorkspace creates a new workspace.
-// The workspace is created with the provided name and billing email.
-func (wr *workspaceRepo) CreateWorkspace(ctx context.Context, workspaceName, billingEmail string, workspaceCreatorUserID uuid.UUID) (models.Workspace, error) {
-	return models.Workspace{}, nil
+func (r *workspaceRepo) CreateWorkspace(ctx context.Context, workspaceName, billingEmail string, workspaceCreatorUserID uuid.UUID) (*models.Workspace, error) {
+	return nil, nil
 }
 
-// ---- Read operations for workspace ----
-
-// GetWorkspaceByWorkspaceID gets a workspace by its ID.
-// This is used to get the workspace details.
-func (wr *workspaceRepo) GetWorkspaceByWorkspaceID(ctx context.Context, workspaceID uuid.UUID) (models.Workspace, error) {
-	return models.Workspace{}, nil
+func (r *workspaceRepo) WorkspaceExists(ctx context.Context, workspaceID uuid.UUID) (bool, error) {
+	return false, nil
 }
 
-// BatchGetWorkspacesByIDs gets workspaces by their IDs.
-// This is used to get the workspace details.
-func (wr *workspaceRepo) BatchGetWorkspacesByIDs(ctx context.Context, workspaceIDs []uuid.UUID) ([]models.Workspace, error) {
-	return []models.Workspace{}, nil
+func (r *workspaceRepo) GetWorkspaceByWorkspaceID(ctx context.Context, workspaceID uuid.UUID) (*models.Workspace, error) {
+	return nil, nil
 }
 
-// GetWorkspaceUserIDs gets the users of the workspace.
-// This is used to get the users of the workspace.
-func (wr *workspaceRepo) GetWorkspaceUserIDs(ctx context.Context, workspaceID uuid.UUID) ([]uuid.UUID, error) {
-	return []uuid.UUID{}, nil
+func (r *workspaceRepo) BatchGetWorkspacesByIDs(ctx context.Context, workspaceIDs []uuid.UUID) ([]models.Workspace, error) {
+	return nil, nil
 }
 
-// GetWorkspaceUserCountByRole gets the count of users by role in the workspace.
-// This is used to get the count of users by role in the workspace.
-func (wr *workspaceRepo) GetWorkspaceUserCountByRole(ctx context.Context, workspaceID uuid.UUID) (map[models.UserWorkspaceRole]int, error) {
-	return map[models.UserWorkspaceRole]int{}, nil
+func (r *workspaceRepo) GetWorkspaceMembers(ctx context.Context, workspaceID uuid.UUID) ([]models.PartialWorkspaceUser, error) {
+	return nil, nil
 }
 
-// GetWorkspaceForUser gets the workspace for a user.
-// This is used to get the workspace for a user.
-func (wr *workspaceRepo) GetWorkspaceForUser(ctx context.Context, userID, workspaceID uuid.UUID) (models.Workspace, error) {
-	return models.Workspace{}, nil
+func (r *workspaceRepo) GetWorkspaceMemberByUserID(ctx context.Context, workspaceID, userID uuid.UUID) (*models.PartialWorkspaceUser, error) {
+	return nil, nil
 }
 
-// ---- Update operations for workspace ----
-
-// User Specific Update Operations
-
-// BatchAddUsersToWorkspace adds a batch of users to workspace
-func (wr *workspaceRepo) BatchAddUsersToWorkspace(ctx context.Context, userIDs []uuid.UUID, workspaceID uuid.UUID) ([]models.WorkspaceUser, error) {
-	return []models.WorkspaceUser{}, nil
+func (r *workspaceRepo) GetWorkspaceUserCountByRole(ctx context.Context, workspaceID uuid.UUID) (map[models.WorkspaceRole]int, error) {
+	return nil, nil
 }
 
-// AddUserToWorkspace adds a user to a workspace
-func (wr *workspaceRepo) AddUserToWorkspace(ctx context.Context, userID, workspaceID uuid.UUID) (models.WorkspaceUser, error) {
-	return models.WorkspaceUser{}, nil
+func (r *workspaceRepo) GetWorkspacesForUserID(ctx context.Context, userID uuid.UUID) ([]models.Workspace, error) {
+	return nil, nil
 }
 
-// BatchRemoveUsersFromWorkspace removes a batch of users from a workspace
-func (wr *workspaceRepo) BatchRemoveUsersFromWorkspace(ctx context.Context, userIDs []uuid.UUID, workspaceID uuid.UUID) error {
+func (r *workspaceRepo) BatchAddUsersToWorkspace(ctx context.Context, userIDs []uuid.UUID, workspaceID uuid.UUID) ([]models.PartialWorkspaceUser, error) {
+	return nil, nil
+}
+
+func (r *workspaceRepo) AddUserToWorkspace(ctx context.Context, userID, workspaceID uuid.UUID) (*models.PartialWorkspaceUser, error) {
+	return nil, nil
+}
+
+func (r *workspaceRepo) BatchRemoveUsersFromWorkspace(ctx context.Context, userIDs []uuid.UUID, workspaceID uuid.UUID) error {
 	return nil
 }
 
-// RemoveUserFromWorkspace removes a user from a workspace
-func (wr *workspaceRepo) RemoveUserFromWorkspace(ctx context.Context, userID, workspaceID uuid.UUID) error {
+func (r *workspaceRepo) RemoveUserFromWorkspace(ctx context.Context, userID, workspaceID uuid.UUID) error {
 	return nil
 }
 
-// Membership Specific Update Operations
-
-// UpdateUserRoleForWorkspace updates the role of a user in the workspace
-func (wr *workspaceRepo) UpdateUserRoleForWorkspace(ctx context.Context, workspaceID, userID uuid.UUID, role models.UserWorkspaceRole) error {
+func (r *workspaceRepo) UpdateUserRoleForWorkspace(ctx context.Context, workspaceID, userID uuid.UUID, role models.WorkspaceRole) error {
 	return nil
 }
 
-// Workspace Specific Update Operations
-
-// UpdateWorkspaceBillingEmail updates the billing email of the workspace.
-// This is used to update the billing email of the workspace.
-func (wr *workspaceRepo) UpdateWorkspaceBillingEmail(ctx context.Context, workspaceID uuid.UUID, billingEmail string) error {
+func (r *workspaceRepo) UpdateUserMembershipStatusForWorkspace(ctx context.Context, workspaceID, userID uuid.UUID, membershipStatus models.MembershipStatus) error {
 	return nil
 }
 
-// UpdateWorkspaceName updates the name of the workspace.
-// This is used to update the name of the workspace.
-func (wr *workspaceRepo) UpdateWorkspaceName(ctx context.Context, workspaceID uuid.UUID, workspaceName string) error {
+func (r *workspaceRepo) UpdateWorkspaceBillingEmail(ctx context.Context, workspaceID uuid.UUID, billingEmail string) error {
 	return nil
 }
 
-// UpdateWorkspaceDetails updates the details of the workspace.
-// This is used to update the details of the workspace.
-func (wr *workspaceRepo) UpdateWorkspaceDetails(ctx context.Context, workspaceID uuid.UUID, workspaceName, billingEmail string) error {
+func (r *workspaceRepo) UpdateWorkspaceName(ctx context.Context, workspaceID uuid.UUID, workspaceName string) error {
 	return nil
 }
 
-// ---- Delete operations for workspace ----
-
-// DeleteWorkspace deletes a workspace by its ID.
-// This is used to delete the workspace.
-func (wr *workspaceRepo) DeleteWorkspace(ctx context.Context, workspaceID uuid.UUID) error {
+func (r *workspaceRepo) UpdateWorkspaceDetails(ctx context.Context, workspaceID uuid.UUID, workspaceName, billingEmail string) error {
 	return nil
 }
 
-// BatchDeleteWorkspaces deletes workspaces by their IDs.
-// This is used to delete the workspaces.
-func (wr *workspaceRepo) BatchDeleteWorkspaces(ctx context.Context, workspaceIDs []uuid.UUID) error {
+func (r *workspaceRepo) DeleteWorkspace(ctx context.Context, workspaceID uuid.UUID) error {
+	return nil
+}
+
+func (r *workspaceRepo) BatchDeleteWorkspaces(ctx context.Context, workspaceIDs []uuid.UUID) error {
 	return nil
 }
