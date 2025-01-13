702db7c8873dd88eeb20d75affbc3af1434556c9
wizenheimer <xNayanKumar@gmail.com>
2025-01-13 19:33:18 +0530

    feat: revise workflow service interface

--- backend/src/internal/service/workflow/interface.go
+++ backend/src/internal/service/workflow/interface.go
@@ -3,24 +3,44 @@ package workflow
 import (
 	"context"
 
-	api "github.com/wizenheimer/byrd/src/internal/models/api"
+	"github.com/google/uuid"
 	models "github.com/wizenheimer/byrd/src/internal/models/core"
+	"github.com/wizenheimer/byrd/src/internal/service/executor"
 )
 
 // WorkflowService represents the service for managing workflows
 type WorkflowService interface {
 	// Initialize initializes the workflow service
-	Initialize(ctx context.Context) []error
+	// This would start the service and start accepting new jobs
+	// Additionally, it would recover any pre-empted jobs
+	Initialize(ctx context.Context) error
 
-	// StartWorkflow starts a new workflow
-	StartWorkflow(ctx context.Context, wr api.WorkflowRequest) (api.WorkflowResponse, error)
+	// Shutdown stops all running jobs and shuts down the workflow service
+	// This would disable the service from accepting new jobs
+	Shutdown(ctx context.Context) error
 
-	// StopWorkflow stops the workflow
-	StopWorkflow(ctx context.Context, wr api.WorkflowRequest) error
+	// Recover recovers all pre-empted jobs
+	// This would be called during the initialization of the service
+	Recover(ctx context.Context) error
 
-	// GetWorkflow returns the workflow from workflow repository
-	GetWorkflow(ctx context.Context, wr api.WorkflowRequest) (api.WorkflowResponse, error)
+	// AddExecutor registers a new executor to the workflow service
+	// This would be called during the initialization of the service
+	// Raises an error if the executor already exists
+	AddExecutor(workflowType models.WorkflowType, executor executor.WorkflowExecutor) error
 
-	// ListWorkflows returns the list of workflows
-	ListWorkflows(ctx context.Context, ws models.WorkflowStatus, wt models.WorkflowType) ([]api.WorkflowResponse, error)
+	// Submits a new job to the workflow
+	// This would be called by the client to submit a new job
+	Submit(ctx context.Context, workflowType models.WorkflowType) (uuid.UUID, error)
+
+	// Stops a running job in the workflow
+	// This would be called by the client to stop a running job
+	Stop(ctx context.Context, workflowType models.WorkflowType, jobID uuid.UUID) error
+
+	// Gets a running job in the workflow
+	// This would be called by the client to get the status of a running job
+	State(ctx context.Context, workflowType models.WorkflowType, jobID uuid.UUID) (*models.Job, error)
+
+	// List returns the list of workflows
+	// This would be called by the client to get the list of running jobs
+	List(ctx context.Context, workflowType models.WorkflowType, jobStatus models.JobStatus) ([]models.Job, error)
 }
