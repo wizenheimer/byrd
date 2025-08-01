c3a5b98e269d4945808160433b884ce007c2fdba
wizenheimer <xNayanKumar@gmail.com>
2025-08-02 03:29:09 +0530

    chore: rem refs

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -12,22 +12,6 @@ const Navbar = () => {
           <a href="/" className="text-xl font-bold">
             byrd
           </a>
-          <div className="flex space-x-4">
-            <a
-              href="https://github.com/wizenheimer/byrd"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Button className="bg-black text-white hover:bg-black/90">
-                <Github size={16} />
-              </Button>
-            </a>
-            <Link href="/onboarding">
-              <Button className="bg-black text-white hover:bg-black/90">
-                Get started
-              </Button>
-            </Link>
-          </div>
         </div>
       </nav>
     </div>
